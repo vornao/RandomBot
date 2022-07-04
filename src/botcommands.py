66f01f185076ac3468d0425117dca95223e211ac
vornao <migliorluca@gmail.com>
2022-07-04 22:55:45 +0200

    fixed commands

--- src/botcommands.py
+++ src/botcommands.py
@@ -1,41 +1,30 @@
-from http.client import BAD_REQUEST
+from queue import Queue
+from functools import wraps
+from uuid import uuid4
+from telegram.ext import ConversationHandler, CallbackContext
+from telegram.inline.inputtextmessagecontent import InputTextMessageContent
+from telegram import InlineQueryResultCachedAudio, InlineQueryResultCachedDocument, InlineQueryResultCachedGif, InlineQueryResultCachedPhoto, Update, InlineQueryResultArticle, InlineQueryResultCachedSticker
+
+from const import *
+from config import ALLOWED
+from utils import * 
+
+import config as botreplies
 import logging
 import sqlite3
 import random
 
-from queue import Queue
-from functools import wraps
-from uuid import uuid4
-
-from telegram.ext import ConversationHandler, CallbackContext
-from telegram.inline.inputtextmessagecontent import InputTextMessageContent
-from telegram import (
-    InlineQueryResultCachedDocument,
-    InlineQueryResultCachedPhoto,
-    Update,
-    InlineQueryResultArticle,
-    InlineQueryResultCachedSticker,
-)
-
-from const import *
-from config import *
-from utils import *
-
-import config as botreplies
-
-QUEUE: Queue
-DB_READER: sqlite3.Connection
-BOT_ENABLED = {}
-
+queue: Queue
+db_reader: sqlite3.Connection
 
 queryHolder = {}
 
 def init(reader, q):
-    global QUEUE
-    global DB_READER
-    QUEUE = q
-    DB_READER = reader
-
+    global queue 
+    global db_reader
+    global queryHolder
+    queue = q
+    db_reader = reader
 
 
 
@@ -46,74 +35,56 @@ def restricted(handler):
     @wraps(handler)
     def wrapped(update: Update, context, *args, **kwargs):
         user_id = None
-        username = None
+        username =  None
 
         # check if it's a message or an inline query
-        try:
+        try: 
             user_id = update.message.from_user.id
             username = update.message.from_user.username
-        except Exception as e:
-            pass
-            # logging.error(e)
+        except: pass
 
-        try:
+        try: 
             user_id = update.inline_query.from_user.id
             username = update.inline_query.from_user.username
-        except Exception as e:
-            pass
-            # logging.error(e)
-
+        except: pass
+    
         if user_id not in ALLOWED and ALLOWED:
-            logging.critical(
-                f"UNAUTHORIZED USER TRIED TO ACCESS BOT! USERNAME: {username}, UID {user_id}"
-            )
-            return None
+            logging.critical(f'UNAUTHORIZED USER TRIED TO ACCESS BOT! USERNAME: {username}, UID {user_id}')
+            return
         return handler(update, context)
-
     return wrapped
 
 
+
 @restricted
-def start_handler(update: Update, context: CallbackContext) -> None:
+def start_handler(update: Update , context: CallbackContext) -> None:
     """Handle /start command"""
     update.message.reply_text(botreplies.START_MSG)
-    BOT_ENABLED[update.message.chat_id] = True
-    QUEUE.put(
-        {
-            "type": "chat",
-            "id": update.message.chat_id,
-        }
-    )
-    logging.info(
-        "User Started BOT (User: %s, id: %d)",
-        update.message.from_user.username,
-        update.message.from_user.id,
-    )
+    logging.info('User Started BOT (User: %s, id: %d)', update.message.from_user.username, update.message.from_user.id)
 
 
 @restricted
 def send_sticker(update: Update, context: CallbackContext):
     """Handle /sendsticker command"""
-    update.message.reply_sticker(sticker=get_random_sticker(DB_READER, 1)[0])
+    update.message.reply_sticker(sticker=get_random_sticker(db_reader, 1)[0])
 
 
 @restricted
-def send_photo(update: Update, context):
+def send_photo(update: Update, context):   
     """Handle /sendphoto command"""
-    update.message.reply_photo(photo=get_random_photo(DB_READER, 1)[0])
-
+    update.message.reply_photo(photo=get_random_photo(db_reader, 1)[0])
 
 @restricted
-def send_text(update: Update, context):
+def send_text(update: Update, context):   
     """Handle /sendphoto command"""
-    update.message.reply_text(get_random_word(DB_READER, 1)[0])
+    print(update.message.chat_id)
+    update.message.reply_text(get_random_word(db_reader, 1)[0])
 
 
 @restricted
-def send_music(update: Update, context):
-    """Handle /sendmusic command"""
-    update.message.reply_audio(audio=get_random_music(DB_READER, 1)[0])
-
+def send_music(update: Update, context):    
+    """Handle /sendmusic command"""    
+    update.message.reply_audio(audio=get_random_music(db_reader, 1)[0])
 
 @restricted
 def send_gif(update: Update, context):    
@@ -124,48 +95,39 @@ def send_gif(update: Update, context):
 
 handlers = [send_text, send_photo, send_sticker, send_music]
 
+@restricted
+def send_random_message(update: Update , context: CallbackContext):
+
+    reply = get_custom_reply(db_reader, 1, update.message.text)
+    if reply is not None:
+        update.message.reply_text(reply[0])
+    else:
+        """
+        Handle simple message to bot and return random quote;
+        There's 1/9 possibility that bot will return photo or audio instead of text
+        """
+        random.choices(handlers, (90, 20, 10, 20))[0](update, context)    
+
+# enable random messages in chat and groups
+# simply crate function to add chat id in DB table.
+@restricted
+def enable_random_messages(update: Update, context: CallbackContext):
+    queue.put({
+        'type':'enablerandomchat',
+        'value': update.message.chat_id
+    })
+    update.message.reply_text('ora farò il btdo inviando quando meno te lo aspetti messaggi e/o foto')
 
 @restricted
-def send_random_message(update: Update, context: CallbackContext):
-    """
-    Handle simple message to bot and return random quote;
-    There's 1/9 possibility that bot will return photo or audio instead of text
-    """
-
-    global BOT_ENABLED
-
-    if update.message.text in TOGGLE_BOT_KEYWORD:
-        try:
-            BOT_ENABLED[update.message.chat_id] = not BOT_ENABLED[
-                update.message.chat_id
-            ]
-        except KeyError:
-            QUEUE.put(
-                {
-                    "type": "chat",
-                    "id": update.message.chat_id,
-                }
-            )
-            BOT_ENABLED[update.message.chat_id] = False
-
-        update.message.reply_text(random.choice(TOGGLE_BOT_ANSWERS))
-        return
-
-    try:
-        if BOT_ENABLED[update.message.chat_id]:
-            random.choices(handlers, (90, 20, 10, 20))[0](update, context)
-    except KeyError:
-        QUEUE.put(
-            {
-                "type": "chat",
-                "id": update.message.chat_id,
-            }
-        )
-        BOT_ENABLED[update.message.chat_id] = True
-        random.choices(handlers, (90, 20, 10, 20))[0](update, context)
+def disable_random_messages(update: Update, context: CallbackContext):
+    queue.put({
+        'type':'disablerandomchat',
+        'value': update.message.chat_id
+    })
+    update.message.reply_text('ora non farò più il btdo')
 
 
-@restricted
+@restricted 
 def add_word(update: Update, context: CallbackContext):
     """entry point for bot to add word"""
     update.message.reply_text(botreplies.ASK_WORD)
@@ -173,15 +135,16 @@ def add_word(update: Update, context: CallbackContext):
 
 
 def store_word(update: Update, context: CallbackContext):
-    """actually store word in db"""
+    """ actually store word in db """
 
-    QUEUE.put({"type": "word", "value": update.message.text})
+    queue.put({'type' : 'word', 'value': update.message.text})
 
-    logging.info(
-        "User Added Word! (User: %s, id: %d, Word: %s)",
-        update.message.from_user.username,
-        update.message.from_user.id,
-        update.message.text,
+    update.message.reply_text(botreplies.CONTENT_ADDED_MSG)
+
+    logging.info('User Added Word! (User: %s, id: %d, Word: %s)', 
+            update.message.from_user.username, 
+            update.message.from_user.id,
+            update.message.text
     )
 
     return STATE_ADDWORD
@@ -189,16 +152,15 @@ def store_word(update: Update, context: CallbackContext):
 
 @restricted
 def add_sticker(update, context):
-    update.message.reply_text(botreplies.ASK_STICKERS)
+    update.message.reply_text(botreplies.ASK_STICKERS) 
     return STATE_ADDSTICKER
 
 
 def store_sticker(update: Update, context):
-    QUEUE.put(
-        {
-            "type": "sticker",
-            "uid": update.message.sticker.file_unique_id,
-            "tid": update.message.sticker.file_id,
+    queue.put({
+        'type' : 'sticker', 
+        'uid': update.message.sticker.file_unique_id, 
+        'tid': update.message.sticker.file_id
         }
     )
     return STATE_ADDSTICKER
@@ -211,11 +173,10 @@ def add_music(update, context):
 
 
 def store_music(update: Update, context):
-    QUEUE.put(
-        {
-            "type": "music",
-            "uid": update.message.audio.file_unique_id,
-            "tid": update.message.audio.file_id,
+    queue.put({
+        'type' : 'music', 
+        'uid': update.message.audio.file_unique_id, 
+        'tid': update.message.audio.file_id
         }
     )
     return STATE_ADDMUSIC
@@ -228,11 +189,10 @@ def add_photo(update, context):
 
 
 def store_photo(update: Update, context):
-    QUEUE.put(
-        {
-            "type": "photo",
-            "uid": update.message.photo[1].file_unique_id,
-            "tid": update.message.photo[1].file_id,
+    queue.put({
+        'type' : 'photo', 
+        'uid': update.message.photo[1].file_unique_id,
+        'tid': update.message.photo[1].file_id
         }
     )
     return STATE_ADDPHOTO
@@ -255,21 +215,16 @@ def store_gif(update: Update, context):
 
 @restricted
 def direct_store_photo(update: Update, context):
-    update.message.reply_text(random.choice(PHOTO_ADDED_REPLY))
-    QUEUE.put(
-        {
-            "type": "photo",
-            "uid": update.message.photo[1].file_unique_id,
-            "tid": update.message.photo[1].file_id,
+    queue.put({
+        'type' : 'photo', 
+        'uid': update.message.photo[1].file_unique_id,
+        'tid': update.message.photo[1].file_id
         }
     )
 
-
-@restricted
 def stop_conversation(update: Update, context):
     update.message.reply_text(botreplies.CONTENT_ADDED_MSG)
     return ConversationHandler.END
-
 
 ############################
 ### inline query handler ###
@@ -279,39 +234,34 @@ def gifs_inline_query_result(update: Update):
     update.inline_query.answer(get_gif_inline_result(6), cache_time= INLINE_CACHE_TIME)
 
 def stickers_inline_query_result(update: Update):
-    update.inline_query.answer(
-        get_sticker_inline_result(6), cache_time=INLINE_CACHE_TIME
-    )
-
+    update.inline_query.answer(get_sticker_inline_result(6), cache_time= INLINE_CACHE_TIME)
 
 def photos_inline_query_result(update: Update):
-    update.inline_query.answer(get_photo_inline_result(6), cache_time=INLINE_CACHE_TIME)
-
+    update.inline_query.answer(get_photo_inline_result(6), cache_time= INLINE_CACHE_TIME)
 
 def photo_inline_query_result(update: Update):
-    update.inline_query.answer(get_photo_inline_result(1), cache_time=INLINE_CACHE_TIME)
-
+    update.inline_query.answer(get_photo_inline_result(1), cache_time= INLINE_CACHE_TIME)
 
 def song_inline_query_result(update: Update):
-    update.inline_query.answer(get_music_inline_result(1), cache_time=INLINE_CACHE_TIME)
-
+    update.inline_query.answer(get_music_inline_result(1), cache_time= INLINE_CACHE_TIME)
 
 def text_inline_query_result(update: Update):
-    update.inline_query.answer(get_word_inline_result(1), cache_time=INLINE_CACHE_TIME)
-
+    update.inline_query.answer(get_word_inline_result(1), cache_time= INLINE_CACHE_TIME)
 
 funcs = [
     stickers_inline_query_result,
     photo_inline_query_result,
     song_inline_query_result,
     text_inline_query_result,
+    gifs_inline_query_result
 ]
 
 
 answers = {
-    botreplies.INLINE_STICKER_QUERY: stickers_inline_query_result,
-    botreplies.INLINE_PHOTO_QUERY: photos_inline_query_result,
-    botreplies.INLINE_AUDIO_QUERY: song_inline_query_result,
+    botreplies.INLINE_STICKER_QUERY : stickers_inline_query_result,
+    botreplies.INLINE_PHOTO_QUERY   : photos_inline_query_result,
+    botreplies.INLINE_AUDIO_QUERY   : song_inline_query_result,
+    botreplies.INLINE_GIF_QUERY     : gifs_inline_query_result
 }
 
 
@@ -323,14 +273,15 @@ def random_inline_query_result():
 def inlinequery(update: Update, context: CallbackContext):
     """
     Handle inline query
-    """
+    """  
     query = update.inline_query.query
 
     # return random content if no valid query sumbitted.
-    if query in answers:
+    if query in answers: 
         answers[query](update)
-    else:
+    else: 
         random_inline_query_result()(update)
+    
 
 @restricted
 def ask_custom_reply(update: Update, context: CallbackContext):
@@ -354,47 +305,58 @@ def ask_custom_answer(update: Update, context: CallbackContext):
 
 
 def get_music_inline_result(quantity: int) -> list:
-
-    songs = get_random_music(DB_READER, quantity)
+    
+    songs =  get_random_music(db_reader, quantity)
 
     # using document to prevent bot from crashing when mp3 files have no title
     return [
         InlineQueryResultCachedDocument(
-            id=uuid4(),
-            title=botreplies.INLINE_AUDIO_REPLY_TITLE,
-            description=botreplies.INLINE_AUDIO_REPLY_DESC,
-            document_file_id=song,
-        )
-        for song in songs
+        id=uuid4(),
+        title=botreplies.INLINE_AUDIO_REPLY_TITLE,
+        description=botreplies.INLINE_AUDIO_REPLY_DESC,
+        document_file_id=song)
+    for song in songs
     ]
 
 
 def get_word_inline_result(quantity: int) -> list:
-
-    quotes = get_random_word(DB_READER, quantity)
-    return [
+    
+    quotes = get_random_word(db_reader, quantity)
+    return [ 
         InlineQueryResultArticle(
             id=uuid4(),
             title=botreplies.INLINE_QUERY_REPLY,
             description=quote,
-            input_message_content=InputTextMessageContent(quote),
+            input_message_content = InputTextMessageContent(quote)
         )
-        for quote in quotes
-    ]
+    for quote in quotes]
 
 
 def get_photo_inline_result(quantity: int) -> list:
-
-    photos = get_random_photo(DB_READER, quantity)
+    
+    photos =  get_random_photo(db_reader, quantity)
     return [
-        InlineQueryResultCachedPhoto(id=uuid4(), photo_file_id=photo)
-        for photo in photos
-    ]
+        InlineQueryResultCachedPhoto(
+                    id=uuid4(),
+                    photo_file_id= photo)
+                    for photo in photos]
 
 
 def get_sticker_inline_result(quantity: int) -> list:
-    stickers = get_random_sticker(DB_READER, quantity)
+    stickers =  get_random_sticker(db_reader, quantity)
     return [
-        InlineQueryResultCachedSticker(id=uuid4(), sticker_file_id=sticker)
-        for sticker in stickers
-    ]
+        InlineQueryResultCachedSticker(
+        id=uuid4(),
+        sticker_file_id = sticker)
+    for sticker in stickers
+]
+
+def get_gif_inline_result(quantity: int) -> list:
+    gifs =  get_random_gif(db_reader, quantity)
+    return [
+        InlineQueryResultCachedGif(
+        id=uuid4(),
+        gif_file_id = gif)
+    for gif in gifs
+]
+
